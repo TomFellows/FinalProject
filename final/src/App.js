a07fb41893490a2995d1323e4ce267f4cbec903a
Rafay Salahuddin <rafaysalahuddin@Rafays-MacBook-Pro-2.local>
2018-08-13 18:56:44 -0400

    third commit

--- final/src/App.js
+++ final/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar.js'
 import ProfileSidebar from './Components/ProfileSidebar'
+import ConnectionsSidebar from './Components/ConnectionsSidebar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Navbar/>
-       <ProfileSidebar/>
+        <Navbar />
+<div className = "flex">
+        <ProfileSidebar />
+        <ConnectionsSidebar />
+</div>
       </div>
     );
   }
---
+++ final/src/Components/ConnectionsSidebar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../CSS/ConnectionsSidebar.css'
+
+
+
+
+class ConnectionsSidebar extends Component {
+  render() {
+    return (
+      <div className="column2">
+      hello
+      </div>
+    );
+  }
+}
+
+export default ConnectionsSidebar;
