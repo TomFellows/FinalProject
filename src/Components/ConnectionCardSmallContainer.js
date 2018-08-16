f776a634ad786b02010ee7901ea8a8f7bd78d54b
Rafay Salahuddin <rafaysalahuddin@Rafays-MacBook-Pro-2.local>
2018-08-16 11:58:20 -0400

    other user profile formatting

--- src/Components/ConnectionCardSmallContainer.js
+++ src/Components/ConnectionCardSmallContainer.js
@@ -5,7 +5,7 @@ import '../CSS/ConnectionCardSmallContainer.css'
 class ConnectionCardSmallContainer extends Component {
     render() {
         return (
-            <div class="smallContainer">
+            <div className="smallContainer">
             
                 <div className = "row">
                     <div className = "profile2"><ConnectionCardSmall src="Images/guy1.jpg"/></div>
