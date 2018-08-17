e7b81b2d4f4a8d38efc756596f2209a7d4c5046d
Megan <megansvasconcelos@gmail.com>
2018-08-17 17:11:43 -0400

    homepage

--- src/Components/ConnectionCardSmallContainer.js
+++ src/Components/ConnectionCardSmallContainer.js
@@ -8,6 +8,7 @@ class ConnectionCardSmallContainer extends Component {
         this.state = {
             connectedUsers: [],
             usersByStyle: [],
+            usersByLocation: [],
             randomizerSuggested: [],
             randomizerCriteria: []
         }
@@ -16,7 +17,7 @@ class ConnectionCardSmallContainer extends Component {
         this.getSuggestedUsers = this.getSuggestedUsers.bind(this)
     }
     componentDidMount() {
-        if (this.props.which === "criteria") {
+        if (this.props.which === "style" || this.props.which === "seeking") {
          this.getSuggestedUsers()
          console.log("test")
         }
@@ -27,6 +28,7 @@ class ConnectionCardSmallContainer extends Component {
     }
 
     getSuggestedUsers() {
+        if (this.props.which === "style") {
         let bod = JSON.stringify({ style: "experimental" })
         fetch('/getUsersByCriteria', {
             method: 'POST',
@@ -55,13 +57,45 @@ class ConnectionCardSmallContainer extends Component {
                     console.log("invalid userId")
                 }
             })
+        } 
+        if(this.props.which === "seeking") {
+        let bod = JSON.stringify({ seeking: "gig" })
+        fetch('/getUsersByCriteria', {
+            method: 'POST',
+            credentials: 'same-origin',
+            body: bod
+        })
+            .then(x => x.text())
+            .then(responseBody => {
+                let parsedBody = JSON.parse(responseBody)
+                if (parsedBody.success === true) {
+                    let maxNumber = parseInt(this.props.number)
+                    let users = [...parsedBody.result]
+                    if (maxNumber >= users.length) {
+                        maxNumber = users.length
+                    }
+                    let ranArr = []
+                    for (let i = 0; ranArr.length < maxNumber; i++) {
+                        let ran = Math.floor(Math.random() * maxNumber)
+                        if (ranArr.indexOf(ran) < 0) {
+                            ranArr = ranArr.concat(ran)
+                        }
+                    }
+                    this.setState({ usersByLocation: parsedBody.result, randomizerCriteria: ranArr })
+
+                } else {
+                    console.log("invalid userId")
+                }
+            })
+        }
     }
+    
+        
 
     getOtherUserConnections() {
         // let bod = JSON.stringify({this.props.user})
         let bod = JSON.stringify({ userId: 12345 })
         fetch('/getAllConnections', {
-            method: 'GET',
             credentials: 'same-origin',
             
         })
@@ -107,11 +141,8 @@ class ConnectionCardSmallContainer extends Component {
                 styles ={user.styles}
             />)})
         }
-          
-           
-        
               
-        if (this.props.which === "criteria") {
+        if (this.props.which === "style") {
         return this.state.randomizerCriteria.map(ran => {
            user = this.state.usersByStyle[ran]
           console.log(user)  
@@ -125,6 +156,20 @@ class ConnectionCardSmallContainer extends Component {
               
             )})
         }
+        if (this.props.which === "seeking") {
+            return this.state.randomizerCriteria.map(ran => {
+               user = this.state.usersByLocation[ran]
+              console.log("hello")  
+               
+            return (
+                    <ConnectionCardSmall
+                        name={user.firstName + " " + user.lastName}
+                        location={user.location}
+                        styles ={user.styles}
+                    />
+                  
+                )})
+            }
     }   
     
 
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -11,13 +11,18 @@ class Homepage extends Component {
         return (
             <div className='homepage'>
             <div className = "title">Suggested Connections</div>
+            <div className = "spaceOut"><ConnectionCardSmallContainer which="specific" number='4'/></div>
+
             {/* <ConnectionCardLarge src="Images/guy1.jpg"/>
             <br/>
             <ConnectionCardLarge src="Images/guy1.jpg"/>
             <br/>
             <ConnectionCardLarge src="Images/guy1.jpg"/> */}
-           
-            <div className = "spaceOut"><ConnectionCardSmallContainer which="criteria" number='4'/></div>
+
+            <h4>Suggestions by style</h4>
+            <div className = "spaceOut"><ConnectionCardSmallContainer which="style" spec="experimental" number='4'/></div>
+            <h4>Suggestions by location</h4>
+            <div className = "spaceOut"><ConnectionCardSmallContainer which="seeking" spec="gig" number='4'/></div>          
             </div>
         );
     }
