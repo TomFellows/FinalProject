5fc8023419459bce4513001966435ad39c250222
Rafay Salahuddin <rafaysalahuddin@Rafays-MacBook-Pro-2.local>
2018-08-19 17:17:47 -0400

    links added

--- src/Components/ConnectionCardSmall.js
+++ src/Components/ConnectionCardSmall.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react' 
 import '../CSS/ConnectionCardSmall.css'
 import { connect } from 'tls';
+import {Link} from 'react-router-dom'
 
 
 class ConnectionCardSmall extends Component {
@@ -20,7 +21,12 @@ class ConnectionCardSmall extends Component {
                  <img src = "Images/profbg.jpg"  className = "connBackgroundPic"/>
                  <img src = "Images/shaun.jpg" className = "connProfilePic"/>
                  <div className = "infoCont">
+                  {console.log("This is the username",this.props.connections)}
+                <Link className = "link" to={'/OtherUserProfile/' + this.props.username}>
                 <div className="info2">{this.props.name}</div>
+       </Link>
+
+                
                 <div className="info3">Styles: {styles}</div>
                 <div className="info3">Location: {this.props.location}</div>
                 <button className = "connectButton">FOLLOW</button>
